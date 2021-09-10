1b43aa5ca319588f7d1a3f117753e341d3f2829c
Kadeem Baldwin <Kadbaldwin@gmail.com>
2021-09-10 16:16:23 -0500

    fixed placeholder text

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -2,10 +2,10 @@ function InputField(props) {
     return (
         <div className='inputField'>
             <input
-                class='placeholder-gray-100 text-center bg-transparent mix-blend-overlay bg-gray-500 bg-opacity-50 flex shadow-inner font-sans rounded-3xl m-3 p-3 justify-center'
                 className='input'
                 type={props.type}
                 placeholder={props.placeholder}
+                class="bg-transparent placeholder-white"
                 value={props.value}
                 onChange={(e) => props.onChange(e.target.value)}
             />
