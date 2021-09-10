30cdbda502eff4465352c66d914a2e484f39622b
Kadeem Baldwin <Kadbaldwin@gmail.com>
2021-09-10 14:26:42 -0500

    created input field

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import InputField from './components/InputField';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [state, setState] = useState('');
+
+  function setInputValue(val) {
+    val = val.trim();
+    if (val.length > 12) {
+      return;
+    }
+    setState(val)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <p>login</p>
+        <InputField
+          type='text'
+          placeholder="Username"
+          value={state ? state : ''}
+          onChange={(state) => setInputValue(state)}
+        />
 
       </header>
     </div>
---
+++ src/components/InputField.js
@@ -0,0 +1,15 @@
+function InputField(props) {
+    return (
+        <div className='inputField'>
+            <input
+                className='input'
+                type={props.type}
+                placeholder={props.placeholder}
+                value={props.value}
+                onChange={(e) => props.onChange(e.target.value)}
+            />
+        </div>
+    );
+}
+
+export default InputField;
